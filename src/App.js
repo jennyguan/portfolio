fdb58f78ee8e5075c9494aff765f0234299433f8
Jenny Guan <jzhguan@uwaterloo.ca>
2020-09-27 00:45:45 -0400

    Style navbar and logos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './images/logo.svg';
 import { BrowserRouter, Route } from "react-router-dom";
+import logo from './images/banner.gif';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
 import Home from './Home';
@@ -13,25 +13,22 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        {/*Set up Router*/}
         <Route exact path="/" component={Home}/>
         <Route exact path="/about" component={About}/>
         <Route exact path="/resume" component={Resume}/>
         <Route exact path="/contact" component={Contact}/>
 
         <Navbar>
-          <Navbar.Brand className="">
-            <img src={logo} className="logo" alt="Logo Image" height="30px"/>
-            Jenny Guan
+          <Navbar.Brand>
+            <img src={logo} className="logo" alt="Logo Image" height="120px"/>
           </Navbar.Brand>
           <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link active href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/resume">Resume</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar>
-
       </div>
     </BrowserRouter>
   );
