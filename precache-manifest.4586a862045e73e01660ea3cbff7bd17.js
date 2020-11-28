17e912ce4ef29abe396ad443064e1316961be780
Jenny Guan <jzhguan@uwaterloo.ca>
2020-11-27 19:52:08 -0500

    Updates

---
+++ precache-manifest.4586a862045e73e01660ea3cbff7bd17.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eaa9362fd7be036c1b399f34d1dfda2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ebcb3b27f4b56e201ea",
+    "url": "/static/css/main.6a175133.chunk.css"
+  },
+  {
+    "revision": "b7b840019e8a8a0fabb1",
+    "url": "/static/js/2.88837371.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.88837371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ebcb3b27f4b56e201ea",
+    "url": "/static/js/main.768fbb52.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "99b9a534cc9d5b1e1a0a775496a3790b",
+    "url": "/static/media/Resume.99b9a534.svg"
+  },
+  {
+    "revision": "b4a151e0fc7eb271e9c0776e33690d5c",
+    "url": "/static/media/Resume.b4a151e0.pdf"
+  },
+  {
+    "revision": "bdd06c7819d2a6c1c8fa111528cb275c",
+    "url": "/static/media/banner.bdd06c78.gif"
+  },
+  {
+    "revision": "61a900dde1012cd4b60524f087aa476a",
+    "url": "/static/media/download-icon.61a900dd.svg"
+  },
+  {
+    "revision": "6c1e016c9a668115a255f238d816f9bd",
+    "url": "/static/media/email-icon.6c1e016c.svg"
+  },
+  {
+    "revision": "407790888bd217bce7d66f3c9ce9a36d",
+    "url": "/static/media/linkedin-icon.40779088.svg"
+  },
+  {
+    "revision": "070e5f7e8eb50a8457579ec650330ab8",
+    "url": "/static/media/me.070e5f7e.jpg"
+  },
+  {
+    "revision": "57c7b57e1895bf7429939f2b310fcda9",
+    "url": "/static/media/phone-icon.57c7b57e.svg"
+  },
+  {
+    "revision": "82b9b809af1d43e437e15d2df17beadc",
+    "url": "/static/media/scroll-down-icon.82b9b809.svg"
+  },
+  {
+    "revision": "84ee20f5aacedc1220ae61347cefb37f",
+    "url": "/static/media/scroll-up-icon.84ee20f5.svg"
+  }
+]);
